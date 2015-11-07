c1aa3d1c797ee2842902b2f840faac8ce2e8b38b
LiuYihan <641872038@qq.com>
2015-11-07 20:05:31 +0800

    先提交

--- SCPullRefresh/SCPullRefresh/KSActivity/KSActivityIndicatorCookieTerminatorAnimation.m
+++ SCPullRefresh/SCPullRefresh/KSActivity/KSActivityIndicatorCookieTerminatorAnimation.m
@@ -188,9 +188,16 @@ static CGFloat  jawAnimationTime = 0.3f;
 
 - (void)beginRefreshing{
     
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.35 * NSEC_PER_SEC)), dispatch_get_main_queue(),^{
+        
+        self.indexNow = 2;
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.6 target:self selector:@selector(animateLoop) userInfo:nil repeats:YES];
+    });
+
     for (CAShapeLayer *jawLayer in self.jawLayerArray) {
         jawLayer.speed = 1.0;
         jawLayer.timeOffset = 0;
+        jawLayer.fillColor = RGBA(0x39c6d7, 1.0).CGColor;
     }
     
     for (CALayer *cookieLayer in self.cookieLayerArray) {
@@ -198,11 +205,6 @@ static CGFloat  jawAnimationTime = 0.3f;
         cookieLayer.timeOffset = 0;
     }
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(),^{
-    
-        self.indexNow = 2;
-        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.6 target:self selector:@selector(animateLoop) userInfo:nil repeats:YES];
-    });
 }
 
 - (void)endRefreshing{
